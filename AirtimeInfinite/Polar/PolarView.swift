537fa0090015a88027fdb20e65732730788fa7e1
Guillaume Vigneron <vef445@gmail.com>
2025-06-30 07:19:30 +0200

    Fix polar chart bug

--- AirtimeInfinite/Polar/PolarView.swift
+++ AirtimeInfinite/Polar/PolarView.swift
@@ -14,12 +14,19 @@ struct PolarView: UIViewRepresentable {
         let track = main.chartViewProcessor.track
         let useImperial = main.useImperialUnits
 
-        let visibleMinX = main.chartViewProcessor.lineChartView.lowestVisibleX
-        let visibleMaxX = main.chartViewProcessor.lineChartView.highestVisibleX
-        let visibleRange = visibleMinX...visibleMaxX
+        let lowestVisibleX = main.chartViewProcessor.lineChartView.lowestVisibleX
+        let highestVisibleX = main.chartViewProcessor.lineChartView.highestVisibleX
+
+        // Check for NaN or invalid ranges
+        guard lowestVisibleX.isFinite, highestVisibleX.isFinite, lowestVisibleX < highestVisibleX else {
+
+            return
+        }
+
+        let visibleRange = lowestVisibleX...highestVisibleX
 
         // Always update polar chart data
         main.polarViewProcessor.loadTrack(track: track, visibleRange: visibleRange, useImperial: useImperial, highlightedPoint: main.highlightedPoint.point)
-        
     }
+
 }
